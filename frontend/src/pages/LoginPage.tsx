ac1f2de21df83a711f4d316afb9d7c831927178e
Arnob Das <55168385+ArnobDas57@users.noreply.github.com>
2025-06-30 23:16:37 -0400

    Update frontend dependencies

--- frontend/src/pages/LoginPage.tsx
+++ frontend/src/pages/LoginPage.tsx
@@ -17,7 +17,7 @@ import { AuthContext } from "../App";
 
 const LoginPage = () => {
   const { handleLogin } = useContext(AuthContext);
-  const [username, setUsername] = useState("");
+  const [identifier, setIdentifier] = useState("");
   const [password, setPassword] = useState("");
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState("");
@@ -30,7 +30,7 @@ const LoginPage = () => {
 
     try {
       const response = await axiosInstance.post("/login", {
-        username,
+        identifier,
         password,
       });
       handleLogin(response.data);
@@ -44,43 +44,127 @@ const LoginPage = () => {
   };
 
   return (
-    <Box sx={{ padding: 2 }}>
-      <Typography variant="h4" gutterBottom>
-        Login
+    <Box
+      sx={{
+        padding: 3,
+        width: "100%",
+        margin: "auto",
+        justifyItems: "center",
+        textAlign: "center",
+        gap: 2,
+        marginTop: 5,
+      }}
+    >
+      <Typography variant="h3" gutterBottom color="white">
+        Welcome back!
       </Typography>
-      <form onSubmit={handleSubmit}>
-        <TextField
-          label="Username"
-          variant="outlined"
-          fullWidth
-          margin="normal"
-          value={username}
-          onChange={(e) => setUsername(e.target.value)}
-        />
-        <TextField
-          label="Password"
-          type="password"
-          variant="outlined"
-          fullWidth
-          margin="normal"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-        />
-        {error && (
-          <Alert severity="error" sx={{ marginBottom: 2 }}>
-            {error}
-          </Alert>
-        )}
-        <Button
-          type="submit"
-          variant="contained"
-          color="primary"
-          fullWidth
-          disabled={loading}
-        >
-          {loading ? <CircularProgress size={24} /> : "Login"}
-        </Button>
-      </form>
+      <Typography variant="h6" gutterBottom color="white">
+        Sign in to your account
+      </Typography>
+      <Typography variant="body2" color="white" sx={{ marginBottom: 2 }}>
+        Please enter your username and password to continue.
+      </Typography>
+
+      <Box
+        sx={{
+          borderRadius: 2,
+          padding: 2,
+          border: "1px solid rgba(255, 255, 255, 0.2)",
+          backgroundColor: "rgba(139, 116, 215, 0.35)",
+        }}
+      >
+        <form onSubmit={handleSubmit}>
+          <TextField
+            label="Username/Email"
+            required
+            autoFocus
+            variant="filled"
+            InputProps={{
+              style: { color: "white" },
+            }}
+            sx={{
+              "& .MuiFilledInput-root": {
+                backgroundColor: "rgba(67, 37, 165, 0.98)",
+              },
+              "& .MuiInputLabel-root": {
+                color: "white",
+              },
+              "& .MuiInputLabel-root.Mui-focused": {
+                color: "white",
+              },
+            }}
+            fullWidth
+            margin="normal"
+            value={identifier}
+            onChange={(e) => setIdentifier(e.target.value)}
+          />
+          <TextField
+            label="Password"
+            type="password"
+            required
+            autoFocus
+            variant="filled"
+            fullWidth
+            sx={{
+              "& .MuiFilledInput-root": {
+                backgroundColor: "rgba(85, 53, 190, 0.98)",
+              },
+              "& .MuiInputLabel-root": {
+                color: "white",
+              },
+              "& .MuiInputLabel-root.Mui-focused": {
+                color: "white",
+              },
+            }}
+            margin="normal"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            InputProps={{
+              style: { color: "white" },
+              
+            }}
+          />
+
+          {error && (
+            <Alert severity="error" sx={{ marginBottom: 2 }}>
+              {error}
+            </Alert>
+          )}
+          <Button
+            type="submit"
+            variant="contained"
+            sx={{
+              width: "30%",
+              marginTop: 2,
+              marginBottom: 2,
+              padding: "12px 0",
+              background:
+                "linear-gradient(-45deg, #FE6B8B, #FF8E53, #FE6B8B, #FF8E53)",
+              backgroundSize: "400% 400%",
+              ":hover": {
+                backgroundPosition: "100% 50%",
+                boxShadow: "0 4px 20px rgba(255, 105, 135, 0.5)",
+                transform: "scale(1.05)",
+              },
+              animation: "gradientShift 3s ease infinite",
+              "@keyframes gradientShift": {
+                "0%": {
+                  backgroundPosition: "0% 50%",
+                },
+                "50%": {
+                  backgroundPosition: "100% 50%",
+                },
+                "100%": {
+                  backgroundPosition: "0% 50%",
+                },
+              },
+            }}
+            disabled={loading}
+          >
+            {loading ? <CircularProgress size={24} /> : "Login"}
+          </Button>
+        </form>
+      </Box>
     </Box>
   );
 };
