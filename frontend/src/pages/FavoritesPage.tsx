22cba9debc39ed5c4d84adf56fec39a293183ac3
Arnob Das <55168385+ArnobDas57@users.noreply.github.com>
2025-07-02 22:04:28 -0400

    Update FavoritesPage

--- frontend/src/pages/FavoritesPage.tsx
+++ frontend/src/pages/FavoritesPage.tsx
@@ -16,6 +16,7 @@ import { FaGithub } from "react-icons/fa";
 import { Canvas } from "@react-three/fiber";
 import { OrbitControls, useGLTF } from "@react-three/drei";
 import { keyframes } from "@emotion/react";
+import { Navigate, useNavigate } from "react-router-dom";
 
 const gradientAnimation = keyframes`
   0% { background-position: 0% 50%; }
@@ -25,6 +26,8 @@ const gradientAnimation = keyframes`
 
 const FavoritesPage = () => {
   const [repoAmount, setRepoAmount] = useState(0);
+
+  const navigate = useNavigate();
 
   return (
     <Box
@@ -60,6 +63,61 @@ const FavoritesPage = () => {
           {repoAmount} saved repositories
         </Typography>
       </Box>
+
+      <Paper
+        sx={{
+          backgroundColor: "rgba(156, 116, 215, 0.28)", // Made semi-transparent
+          backdropFilter: "blur(10px)",
+          border: "1px solid rgba(255, 255, 255, 0.2)", // Subtle border
+          boxShadow: "0 8px 32px rgba(0, 0, 0, 0.1)", // Soft shadow
+          padding: 3,
+          borderRadius: 2,
+          width: "100%",
+          maxWidth: 400,
+          margin: "auto",
+          display: "flex",
+          flexDirection: "column",
+          gap: 3,
+          marginTop: 10,
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <Typography
+          variant="h6"
+          sx={{
+            background:
+              "linear-gradient(90deg,rgb(48, 247, 204),rgb(246, 206, 255), rgb(48, 247, 204))",
+            WebkitBackgroundClip: "text",
+            WebkitTextFillColor: "transparent",
+            backgroundSize: "200% auto",
+            animation: `${gradientAnimation} 3s linear infinite`,
+          }}
+        >
+          {" "}
+          Discover More Repositories
+        </Typography>
+        <Button
+          onClick={() => {
+            navigate("/search");
+          }}
+          fullWidth
+          variant="contained"
+          sx={{
+            height: "50px",
+            backgroundColor: "rgba(186, 147, 223, 0.8)",
+            color: "white",
+            borderRadius: 1,
+            "&:hover": {
+              backgroundColor: "rgba(0, 0, 0, 0.8)",
+            },
+            flexShrink: 0,
+          }}
+        >
+          <Search sx={{ marginRight: 0.5 }} />
+          Search
+        </Button>
+      </Paper>
     </Box>
   );
 };
