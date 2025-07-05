c01f86ffde6b851cfd8003d8b4f268e2d3cc298f
Arnob Das <55168385+ArnobDas57@users.noreply.github.com>
2025-07-05 18:19:24 -0400

    Update server.ts

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -10,7 +10,7 @@ dotenv.config();
 
 const allowedOrigins = [
   "http://localhost:5173", // local-development
-  "https://repo-explorer-chi.vercel.app/", // frontend URL
+  "https://repo-explorer-chi.vercel.app", // frontend URL
 ];
 
 const PORT = process.env.PORT || 5000;
